38b48f79a70b4bdf6d76d4f4b64347577f8cd454
gaoqi <1224362143@qq.com>
2023-11-27 19:47:35 +0800

    chore: 先把数据转成Object

--- src/func.ts
+++ src/func.ts
@@ -1,19 +1,44 @@
 import path = require("path");
 import * as vscode from "vscode";
 import * as xlsx from "xlsx";
+import * as fs from "fs";
+import { getInitConstants } from "./initConstants";
 
 export const funcs = {
   // xlsx转ts
   xlsxToTs: (document: vscode.Uri) => {
     try {
-      console.log("xlsx======>xlsxToTs");
       //   const pathFile = await vscode.workspace.openTextDocument(document.fsPath);
       (global as any).xlsx = xlsx;
       (global as any).pathFile = document;
-      const file = xlsx.read(document.path);
-      console.log(file, "xlsx======data");
+      const { xlsxTransformPath } = getInitConstants();
+      const data = fs.readFileSync(document.fsPath).buffer;
+      const file = xlsx.read(data);
+      const l10n: Record<string, Record<string, string>> = {};
       for (const sheetName of file.SheetNames) {
-        const data = xlsx.utils.sheet_to_json(file.Sheets[sheetName]);
+        const sheetData: any = xlsx.utils.sheet_to_json(file.Sheets[sheetName]);
+        const fields = Object.keys(sheetData[0]);
+
+        // 获取到xlsx里的数据转成Object格式
+        sheetData.forEach((word: Record<string, string>) => {
+          // 0 是 key
+          for (let index = 1; index < fields.length; index++) {
+            const field = fields[index];
+            if (!l10n[field]) {
+              l10n[field] = {};
+            }
+            l10n[field][word.key] = String(word[field || "zh-CN"]) || "";
+          }
+        });
+        console.log(document, "document");
+        // 将Object转成ts
+        // Object.keys(l10n).forEach((key: string) => {
+        //   const str = `export default ${JSON.stringify(l10n[key], null, 2)}\n`
+        //   const writePath = getLangTsPath(key)
+        //   prettier.format(str, { ...config, semi: false, filepath: writePath }).then((res: string) => {
+        //     fs.writeFileSync(writePath, res, 'utf8')
+        //   })
+        // })
       }
     } catch (err) {
       console.log(err, "xlsx======error");
--- src/register.ts
+++ src/register.ts
@@ -5,7 +5,6 @@ export default class Extension {
 
   public registerCommands(context: vscode.ExtensionContext): void {
     commands.forEach((item) => {
-      console.log(item, "xlsx======>注册事件");
       const command = vscode.commands.registerCommand(item.key, item.func);
       context.subscriptions.push(command);
     });
